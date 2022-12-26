b6f93a5809b38effa88be5d6dff4ad906b0d042c
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2022-12-26 14:14:03 +0000

    variables(const,let,var)

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+
+const accountId = 12121;
+let accountEmail = "anuj@gmail.com";
+var accountPassword = "1234";
+accountCity = "jaipur";
+
+let accountState; // it  will print undefined 
+
+/*
+    prefer not to use var
+    because of the issue in block scope and functional scope
+*/
+
+
+// accountId = 2; // not allowed as constant(const) cannot be changed
+//console.log(accountID);
+
+accountEmail = "cmrit@gmail.com";
+accountPassword = "54321";
+accountCity = "Bangaluru";
+
+//prints normal output
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+//THIS WILL PRINT ALL VALUES IN TABULAR FORM
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
