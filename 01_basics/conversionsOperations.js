4fe2f4eef3ca690d657c7684ea45c64d8e907506
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2022-12-30 19:58:25 +0000

    conversions of DATATYPES

---
+++ 01_basics/conversionsOperations.js
@@ -0,0 +1,30 @@
+let score = "anuj"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueINnumber = Number(score);
+// console.log(typeof valueINnumber);
+// console.log( valueINnumber); // NaN (NOT A NUMBER)
+
+//NOTES
+// 33 => 33
+// "33abc" => NaN (not a number) but type is number
+// true =>1; false => 0;
+
+let isLoggedIN = "ANUJ"
+//corverting in boolean
+let booleanisLoggedIn = Boolean(isLoggedIN)
+//console.log(booleanisLoggedIn);
+
+//NOTES
+// 1 => true; 0 => false
+// "" => flase  
+//"ANUJ" => true
+
+let AnyNumber = 33
+//changed into string
+
+let NumberTostring = String(AnyNumber)
+console.log(NumberTostring);
+console.log(typeof NumberTostring); 
