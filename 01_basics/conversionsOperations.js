3036623488cbdd602f9b75472a1640feaf74d831
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2023-05-06 01:32:40 +0530

    Added OPERATIONS part

--- 01_basics/conversionsOperations.js
+++ 01_basics/conversionsOperations.js
@@ -26,5 +26,38 @@ let AnyNumber = 33
 //changed into string
 
 let NumberTostring = String(AnyNumber)
-console.log(NumberTostring);
-console.log(typeof NumberTostring); 
+// console.log(NumberTostring);
+// console.log(typeof NumberTostring); 
+
+
+// *****************************OPERATIONS***************************************
+
+let value = 3;
+let Negvalue = -value;
+// console.log(Negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "ANUJ"
+
+let str3 = str1 + " " +  str2;
+// console.log(str3);
+
+// console.log(1 + "2");  // first element  datatype will affect the output
+//these are not good practice (bad code)
+// console.log("1" + 2);
+// console.log(1 + 2 + "3");
+// console.log("1"+ 2 + 3); // if string 1st then all output will affected by it
+
+//Read mdn incrementer documentry for better understanding on this topic
+let counter = 99;
+counter++;
+console.log(counter);
+
+
