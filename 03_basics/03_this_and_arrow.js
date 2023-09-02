ea158c1cd6770801f744238755acc54cb25bfca9
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2023-09-03 01:17:08 +0530

    Update 03_this_and_arrow.js

--- 03_basics/03_this_and_arrow.js
+++ 03_basics/03_this_and_arrow.js
@@ -20,6 +20,14 @@ const user = {
                     // so broweser has global object as (window object)
 
 
+// The output of console.log(this) in the browser and Node.js environment in JavaScript depends on the context in which it is used.
+
+// In the browser, this refers to the window object. So, the output of console.log(this) in the browser will be the window object.
+
+// In Node.js, this refers to the global object. So, the output of console.log(this) in Node.js will be the global object.
+
+
+
 // function chai(){
 //     let username  = "Anuj"
 //     console.log(this.username);
