ec0b43e8137eaae555f1ead638173da910352e95
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2023-10-26 09:20:51 +0000

    added Rock paper scissor game in Math random file

--- 12_Random_number_generation_inJS/random.js
+++ 12_Random_number_generation_inJS/random.js
@@ -67,3 +67,14 @@ function rollDice() {
 console.log( rollDice() )
 
 
+//Rock, Paper , scissor game 
+let hands = ["rock", "paper", "scissor"]
+
+// Create a function that returns a random item from the array
+
+function getHand() {
+    let randomIndex = Math.floor( Math.random() * 3)
+    return hands[randomIndex]
+}
+
+console.log( getHand() ) 
